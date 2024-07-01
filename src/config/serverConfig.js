685aa536b4e76780b8ebacc991e19639b9d63cb7
hrisabhy <hrisabhyadav31@gmail.com>
2024-07-01 21:48:38 +0530

    Signup logic with password encryption

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,7 +1,9 @@
 const path = require("path");
+const bcrypt = require("bcryptjs");
 
 require('dotenv').config({path: path.resolve(__dirname, "../../.env")})
 
 module.exports = {
     PORT: process.env.PORT,
+    SALT: bcrypt.genSaltSync(10)
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require('bcryptjs');
+
 const {PORT} = require("./config/serverConfig");
+const {User} = require("./models/index");
+const ApiRouter = require("./routes/index");
+
 const app = express();
 
 const prepareAndStartServer = () => {    
-    app.listen(PORT, () => {
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended: true}))
+
+    app.use('/api', ApiRouter);
+    
+    app.listen(PORT, async () => {
+        const user = await User.findByPk(2);
+        const incomingPw = 'ilovepavvu'
+        const response = bcrypt.compareSync(incomingPw, user.password)
+        console.log(response)
         console.log(`Server started at ${PORT}`);
     })
 }
