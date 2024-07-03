2288d847d75374bef3e4d1aecd38ebbd76ffffa4
hrisabhy <hrisabhyadav31@gmail.com>
2024-07-03 09:36:54 +0530

    Added getUserById in repository layer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 
 const {PORT} = require("./config/serverConfig");
 const {User} = require("./models/index");
+const UserRepository = require("./repository/user-repository");
 const ApiRouter = require("./routes/index");
 
 const app = express();
@@ -15,10 +16,15 @@ const prepareAndStartServer = () => {
     app.use('/api', ApiRouter);
     
     app.listen(PORT, async () => {
-        const user = await User.findByPk(2);
-        const incomingPw = 'ilovepavvu'
-        const response = bcrypt.compareSync(incomingPw, user.password)
-        console.log(response)
+        // const user = await User.findByPk(2);
+        // const incomingPw = 'ilovepavvu'
+        // const response = bcrypt.compareSync(incomingPw, user.password)
+        // console.log(response)
+
+        // const userRepo = new UserRepository();
+        // const user = await userRepo.getById(1);
+        // console.log(user)
+
         console.log(`Server started at ${PORT}`);
     })
 }
--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -22,6 +22,18 @@ class UserRepository {
             throw error;
         }
     }
+
+    async getById(userId) {
+        try {
+            const user = await User.findByPk(userId, {
+                attributes: ['email', 'id']
+            })
+            return user
+        } catch (error) {
+            console.log("Something went wrong on the repository layer")
+            throw error
+        }
+    }
 }
 
 module.exports = UserRepository;
